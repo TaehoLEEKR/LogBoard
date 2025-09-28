9c0dcb5329b8780dd8ce73ec19305af352a740af
TaehoLEEKR <679748@naver.com>
2025-09-28 15:50:42 +0900

    chore : test server B

---
+++ serverB/src/test/java/com/open/serverb/ServerBApplicationTests.java
@@ -0,0 +1,14 @@
+package com.open.serverb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerBApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
