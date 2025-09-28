4db9fa5e120d0e24b4d3d1dfafc1201a488208b2
TaehoLEEKR <679748@naver.com>
2025-09-28 14:35:34 +0900

    first-init

---
+++ src/main/java/com/open/logboard_grpc/LogBoardGRpcApplication.java
@@ -0,0 +1,14 @@
+package com.open.logboard_grpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogBoardGRpcApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(LogBoardGRpcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/open/logboard_grpc/LogBoardGRpcApplicationTests.java
@@ -0,0 +1,14 @@
+package com.open.logboard_grpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogBoardGRpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
